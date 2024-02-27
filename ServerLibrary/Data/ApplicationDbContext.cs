3a78f0530135c99d991acf9119be07ff0dbf20fe
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-27 21:26:44 +0700

    add login and register response

--- ServerLibrary/Data/ApplicationDbContext.cs
+++ ServerLibrary/Data/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace ServerLibrary.Data
         public DbSet<Branch> Branches { get; set; }
         public DbSet<Town> Towns { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<SystemRole> SystemRoles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
 
     }
 }
