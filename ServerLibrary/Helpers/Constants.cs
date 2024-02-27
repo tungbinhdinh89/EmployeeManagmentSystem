725bc59521017163b38d06c961ff5260093dbdfc
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-27 23:02:11 +0700

    add constants

---
+++ ServerLibrary/Helpers/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace ServerLibrary.Helpers
+{
+    public class Constants
+    {
+        public static string Admin { get; } = "Admin";
+        public static string User { get; } = "User";
+
+    }
+}
