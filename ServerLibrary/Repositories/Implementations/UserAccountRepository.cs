04570ce2648af24f85dd920271a2e0b741a46836
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-28 10:21:35 +0700

    check role, generate token, and provide token

--- ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -3,9 +3,14 @@ using BaseLibrary.Entities;
 using BaseLibrary.Responses;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using ServerLibrary.Data;
 using ServerLibrary.Helpers;
 using ServerLibrary.Repositories.Contracts;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using Constants = ServerLibrary.Helpers.Constants;
 
 namespace ServerLibrary.Repositories.Implementations
@@ -42,7 +47,7 @@ namespace ServerLibrary.Repositories.Implementations
             if (checkUserRole is null)
             {
                 response = await AddToDatabase(new SystemRole() { Name = Constants.User });
-                await AddToDatabase(new UserRole() { RoleId =response.Id, UserId = applicationUser.Id });
+                await AddToDatabase(new UserRole() { RoleId = response.Id, UserId = applicationUser.Id });
             }
             else
             {
@@ -51,14 +56,59 @@ namespace ServerLibrary.Repositories.Implementations
             return new GeneralResponse(true, "Account created!");
         }
 
-        public Task<LoginResponse> SignInAsync(Login user)
+        public async Task<LoginResponse> SignInAsync(Login user)
         {
-            throw new NotImplementedException();
+            if (user == null) return new LoginResponse(false, "Model is empty");
+
+            var applicationUser = await FindUserByEmail(user.Email!);
+            if (applicationUser is null) return new LoginResponse(false, "User not found");
+
+            // Verify password
+            if (!BCrypt.Net.BCrypt.Verify(user.Password, applicationUser.Password))
+                return new LoginResponse(false, "Email/Password not valid");
+
+            // Check role
+            var getUserRole = await appDbContext.UserRoles.FirstOrDefaultAsync(u => u.UserId == applicationUser.Id);
+            if (getUserRole is null) return new LoginResponse(false, "User role not found");
+
+            var getRoleName = await appDbContext.SystemRoles.FirstOrDefaultAsync(u => u.Id == getUserRole.RoleId);
+            if (getUserRole is null) return new LoginResponse(false, "User role not found");
+
+            // Provide token
+            string jwtToken = GenerateToken(applicationUser, getRoleName!.Name!);
+            string refreshToken = GenerateRefreshToken();
+            return new LoginResponse(true, "Login successfully", jwtToken, refreshToken);
         }
 
+        // Generate token
+        private string GenerateToken(ApplicationUser user, string role)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.Value.Key!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var userClaims = new[]
+            {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.FullName!),
+                    new Claim(ClaimTypes.Email, user.Email!),
+                    new Claim(ClaimTypes.Role, role!)
+                };
+            var token = new JwtSecurityToken(
+                issuer: config.Value.Issuer,
+                audience: config.Value.Audience,
+                claims: userClaims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private string GenerateRefreshToken() => Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
+
+        // Find user by email
         private async Task<ApplicationUser> FindUserByEmail(string email) =>
             await appDbContext.ApplicationUsers.FirstOrDefaultAsync(e => e.Email!.ToLower()!.Equals(email!.ToLower()));
 
+        // Add data to db
         private async Task<T> AddToDatabase<T>(T model)
         {
             var result = appDbContext.Add(model!);
