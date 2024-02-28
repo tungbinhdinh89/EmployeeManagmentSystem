f6fd46068b58016d6c4b06285bddbb4b0b99b485
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-28 22:44:06 +0700

    add method refresh token to interface

--- ServerLibrary/Repositories/Contracts/IUserAccount.cs
+++ ServerLibrary/Repositories/Contracts/IUserAccount.cs
@@ -7,5 +7,6 @@ namespace ServerLibrary.Repositories.Contracts
 
         Task<GeneralResponse> CreateAsync(Register user);
         Task<LoginResponse> SignInAsync(Login user);
+        Task<LoginResponse> RefreshTokenAsync(RefreshToken token);
     }
 }
