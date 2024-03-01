2925e09daea5c72d433ca954b6d04d792e285185
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-03-01 07:47:17 +0700

    add interface user account service and method implemet

---
+++ ClientLibrary/Services/Implementations/UserAccountService.cs
@@ -0,0 +1,30 @@
+﻿
+using BaseLibrary.DTOs;
+using BaseLibrary.Responses;
+using ClientLibrary.Services.Contracts;
+
+namespace ClientLibrary.Services.Implementations
+{
+    internal class UserAccountService : IUserAccountService
+    {
+        Task<GeneralResponse> IUserAccountService.CreateAsync(Register user)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<LoginResponse> IUserAccountService.SignInAsync(Login user)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<LoginResponse> IUserAccountService.RefreshTokenAsync(RefreshToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<WeatherForecast[]> IUserAccountService.GetWeatherForecasts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
