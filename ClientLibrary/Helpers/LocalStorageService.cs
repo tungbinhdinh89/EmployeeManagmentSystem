44afe313651e37a98dc17af290ab0908587c78a3
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-03-01 20:52:24 +0700

    add method to authentication user

---
+++ ClientLibrary/Helpers/LocalStorageService.cs
@@ -0,0 +1,13 @@
+﻿
+using Blazored.LocalStorage;
+
+namespace ClientLibrary.Helpers
+{
+    public class LocalStorageService(ILocalStorageService localStoreageService)
+    {
+        private const string StorageKey = "authentication-token";
+        public async Task<string> GetToken() => await localStoreageService.GetItemAsStringAsync(StorageKey);
+        public async Task SetToken(string item) => await localStoreageService.SetItemAsStringAsync(StorageKey, item);
+        public async Task RemoveToken() => await localStoreageService.RemoveItemAsync(StorageKey);
+    }
+}
