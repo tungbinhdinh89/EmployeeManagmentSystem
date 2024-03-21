f02ca1ee26fdd23fb1fae98a1d6d75b1b430ccf1
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-03-21 16:21:24 +0700

    create allState

---
+++ Client/ApplicationStates/AllState.cs
@@ -0,0 +1,92 @@
+﻿namespace Client.ApplicationStates
+{
+    public class AllState
+    {
+        // Scope action
+        public Action? Action { get; set; }
+
+        // General Department
+        public bool ShowGeneralDepartment { get; set; }
+        public void GeneralDepartmentClicked()
+        {
+            ResetAllDepartments();
+            ShowGeneralDepartment = true;
+            Action?.Invoke();
+        }
+
+        // Department
+        public bool ShowDepartment { get; set; }
+        public void DepartmentClicked()
+        {
+            ResetAllDepartments();
+            ShowDepartment = true;
+            Action?.Invoke();
+        }
+
+        // Branch
+        public bool ShowBranch { get; set; }
+        public void BranchClicked()
+        {
+            ResetAllDepartments();
+            ShowBranch = true;
+            Action?.Invoke();
+        }
+
+        // Country
+        public bool ShowCountry { get; set; }
+        public void CountryClicked()
+        {
+            ResetAllDepartments();
+            ShowCountry = true;
+            Action?.Invoke();
+        }
+
+        // City
+        public bool ShowCity { get; set; }
+        public void CityClicked()
+        {
+            ResetAllDepartments();
+            ShowCity = true;
+            Action?.Invoke();
+        }
+
+        // Town
+        public bool ShowTown { get; set; }
+        public void TownClicked()
+        {
+            ResetAllDepartments();
+            ShowTown = true;
+            Action?.Invoke();
+        }
+
+        // User
+        public bool ShowUser { get; set; }
+        public void UserClicked()
+        {
+            ResetAllDepartments();
+            ShowUser = true;
+            Action?.Invoke();
+        }
+
+        // Employee
+        public bool ShowEmployee { get; set; }
+        public void EmployeeClicked()
+        {
+            ResetAllDepartments();
+            ShowEmployee = true;
+            Action?.Invoke();
+        }
+
+        private void ResetAllDepartments()
+        {
+            ShowGeneralDepartment = false;
+            ShowDepartment = false;
+            ShowBranch = false;
+            ShowCountry = false;
+            ShowCity = false;
+            ShowTown = false;
+            ShowUser = false;
+            ShowEmployee = false;
+        }
+    }
+}
