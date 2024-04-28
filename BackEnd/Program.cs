378f47d01d96ea32a1e235359a8185460de37c5b
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-04-28 07:52:01 +0700

    add employee service

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -52,6 +52,7 @@ builder.Services.AddScoped<IGenericRepositoryInterface<Branch>, BranchRepository
 builder.Services.AddScoped<IGenericRepositoryInterface<Country>, CountryRepository>();
 builder.Services.AddScoped<IGenericRepositoryInterface<City>, CityRepository>();
 builder.Services.AddScoped<IGenericRepositoryInterface<Town>, TownRepository>();
+builder.Services.AddScoped<IGenericRepositoryInterface<Employee>, EmployeeRepository>();
 
 builder.Services.AddCors(options =>
 {
---
+++ ServerLibrary/Repositories/Implementations/EmployeeRepository.cs
@@ -0,0 +1,85 @@
+﻿using BaseLibrary.Entities;
+using BaseLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+using ServerLibrary.Repositories.Contracts;
+
+namespace ServerLibrary.Repositories.Implementations
+{
+    public class EmployeeRepository(ApplicationDbContext appDbContext) : IGenericRepositoryInterface<Employee>
+    {
+        public async Task<GeneralResponse> DeleteById(int id)
+        {
+            var item = await appDbContext.Employees.FindAsync(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            appDbContext.Employees.Remove(item);
+            await Commit();
+            return Success();
+        }
+
+        public async Task<List<Employee>> GetAll()
+        {
+            var employees = await appDbContext.Employees
+                .AsNoTracking()
+                .Include(t => t.Town)
+                .ThenInclude(c => c.City)
+                .ThenInclude(c => c.Country)
+                .Include(b => b.Branch)
+                .ThenInclude(d => d.Department)
+                .ThenInclude(gd => gd.GeneralDepartment)
+                .ToListAsync();
+            ;
+            return employees;
+        }
+
+        public async Task<Employee> GetById(int id)
+        {
+            var employee = await appDbContext.Employees.FindAsync(id);
+            if (employee is null) { return new Employee(); }
+            return employee;
+        }
+
+        public async Task<GeneralResponse> Insert(Employee employee)
+        {
+            if (!await CheckName(employee.Name!)) return new GeneralResponse(false, "Employee already added!");
+
+            await appDbContext.Employees.AddAsync(employee);
+            await Commit();
+            return Success();
+        }
+
+        public async Task<GeneralResponse> Update(Employee employee)
+        {
+            var findUser = await appDbContext.Employees.FindAsync(employee.Id);
+            if (findUser is null) { return NotFound(); }
+
+            findUser.Name = employee.Name;
+            findUser.Other = employee.Other;
+            findUser.Address = employee.Address;
+            findUser.TelephoneNumber = employee.TelephoneNumber;
+            findUser.BranchId = employee.BranchId;
+            findUser.TownId = employee.TownId;
+            findUser.CivilId = employee.CivilId;
+            findUser.FileNumber = employee.FileNumber;
+            findUser.JobName = employee.JobName;
+            findUser.Photo = employee.Photo;
+
+            appDbContext.Employees.Update(findUser);
+            await Commit();
+            return Success();
+        }
+
+        private static GeneralResponse NotFound() => new(false, "Employee does not exist");
+        private static GeneralResponse Success() => new(true, "Process completed");
+        private async Task Commit() => await appDbContext.SaveChangesAsync();
+        private async Task<bool> CheckName(string name)
+        {
+            var item = await appDbContext.Employees.FirstOrDefaultAsync(x => x.Name!.ToLower().Trim().Equals(name.ToLower().Trim()));
+            return item is null;
+        }
+    }
+}
